3021a35015d1d74a957c5d6be5cc8a813e060c14
nl-hugo <info@hugojanssen.nl>
2015-06-04 13:05:19 +0200

    Added plot3

---
+++ plot3.R
@@ -0,0 +1,40 @@
+#
+# Coursera | Exploratory Data Analysis
+# Project 1
+# plot3.R
+#
+
+# Check if data directory exists, create one if needed
+if (!file.exists("data")) {
+  dir.create("data")
+}
+
+# Download the dataset and unzip the files
+if (!file.exists("./data/household_power_consumption.txt")) {
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, destfile="./data/dataset.zip")
+  unzip("./data/household_power_consumption.txt", exdir="./data")
+}
+
+# Read the data
+d <- read.table(file="./data/household_power_consumption.txt", header=TRUE, sep=";", nrows=69516, na.strings="?", stringsAsFactors=FALSE)
+
+# Convert to Date and Time
+d$Date <- as.Date(d$Date, "%d/%m/%Y")
+d$DateTime <- as.POSIXct(paste(d$Date, d$Time))
+
+# Select data for 2007-02-01 and 2007-02-02 only
+d <- subset(d, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+# Plot the line chart
+plot(d$Sub_metering_1~d$DateTime, type="l",
+     ylab="Energy sub metering", 
+     xlab="")
+lines(d$Sub_metering_2~d$DateTime, col="red")
+lines(d$Sub_metering_3~d$DateTime, col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),  
+       col=c("black","blue","red"), lwd=3);
+
+# Save plot to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
